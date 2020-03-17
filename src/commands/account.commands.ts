a34a17cc39a3c0710cc811749da40e03c31883c8
EdixonAlberto <edixonalbertto@gmail.com>
2020-03-17 14:42:40 -0400

    improving response in balance
    - commenting code in account.commands

--- src/commands/account.commands.ts
+++ src/commands/account.commands.ts
@@ -4,9 +4,10 @@ import DataForRequest from '../modules/DataForRequest';
 import { getAccount } from '../routes';
 import Utils from '../modules/Utils';
 
+/* CONFIG */
 const DEFAULT_ASSET: TAsset = config.defaultAsset;
 const DECIMAL_QTY_FIAT: number = config.decimalQty.fiat;
-const DEFAULT_FREE: number = 0.001;
+const DEFAULT_FREE: number = 0.001; // TODO: Esto debe venir desde las config dinamicas
 
 export const balance = async (content: TContent, response: BotResponse): Promise<any> => {
   if (content.command === commandsList.balance) {
@@ -17,36 +18,44 @@ export const balance = async (content: TContent, response: BotResponse): Promise
       const account: TResponses['account'] = await getAccount(dataRequest);
 
       if (asset === 'LIST') {
+        // Show list the assets with balance and >= DEFAULT_FREE
         const balancesList: Array<TBalance> = account.balances.filter(
           (balance: TBalance) => balance.free >= DEFAULT_FREE
         );
-        let container: number = 0;
 
+        let patrimony: number = 0; // Accumulator the money
+
+        // Creating table the balances
         if (balancesList.length > 0) {
-          const tableBalances: TTable = balancesList.map((balance: TBalance) => {
+          const balancesTable: TTable = balancesList.map((balance: TBalance) => {
+            patrimony += Number(balance.free) + Number(balance.locked);
+
             const free = Utils.Float.convert(balance.free, DECIMAL_QTY_FIAT);
             const locked = Utils.Float.convert(balance.locked, DECIMAL_QTY_FIAT);
-            container += Number(free) + Number(locked);
 
+            // Adding columns in the table
             return {
               title: balance.asset,
-              content: `
-                free: ${free} $
-                locked: ${locked} $
-              `
+              content: `free: ${free} $ \nlocked: ${locked} $`,
+              fieldType: 'column'
             };
           });
 
-          const patrimonio = Utils.Float.convert(container, DECIMAL_QTY_FIAT);
+          // Adding a last row in the table
+          balancesTable.push({
+            title: 'Saldo de todos tus activos',
+            content: Utils.Float.convert(patrimony, DECIMAL_QTY_FIAT) + ' $',
+            fieldType: 'row'
+          });
 
           response.embeded({
             header: 'SALDO',
-            title: 'Lista de activos con saldo en tu cuenta',
-            detail: tableBalances,
-            footer: `El saldo de todos tus activos suma **${patrimonio} $**`
+            title: 'Lista de activos con saldo',
+            detail: balancesTable
           });
         } else response.general(`No hay activos con saldo > ${DEFAULT_FREE} $`);
       } else {
+        // Show asset seleted
         const balance = account.balances.find(
           (balance: TBalance) => balance.asset === asset
         );
@@ -55,13 +64,12 @@ export const balance = async (content: TContent, response: BotResponse): Promise
           const free = Utils.Float.convert(balance.free, DECIMAL_QTY_FIAT);
           const locked = Utils.Float.convert(balance.locked, DECIMAL_QTY_FIAT);
 
-          response.general(`
-            **Saldo del activo: ${asset}**
-            free: ${free} $
-            locked: ${locked} $
-          `);
-        } else {
-        }
+          response.embeded({
+            header: 'SALDO',
+            title: `Saldo del activo: ${asset}`,
+            detail: `free: ${free} $ \nlocked: ${locked} $`
+          });
+        } else response.general(`No hay saldo ${asset}`);
       }
     } catch (error) {
       console.error(`>> ERROR-${content.command.toUpperCase()} -> ${error}`);
