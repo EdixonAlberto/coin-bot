a7ad7b9fc179ed0d5b085db2ccd9e9474f299926
EdixonAlberto <edixonalbertto@gmail.com>
2020-03-24 11:04:00 -0400

    refactoring commands (account, coin)

--- src/commands/account.commands.ts
+++ src/commands/account.commands.ts
@@ -1,78 +1,75 @@
-import { commandsList } from '../enumerations';
 import BotResponse from '../modules/BotResponse';
 import DataForRequest from '../modules/DataForRequest';
 import { getAccount } from '../routes';
 import Utils from '../modules/Utils';
 
 /* CONFIG */
-const DEFAULT_ASSET: TAsset = config.defaultAsset;
-const DECIMAL_QTY_FIAT: number = config.decimalQty.fiat;
+const DEFAULT_ASSET: TAsset = 'BTC';
+const DECIMAL_QTY_FIAT: number = 2;
 const DEFAULT_FREE: number = 0.001; // TODO: Esto debe venir desde las config dinamicas
 
 export const balance = async (content: TContent, response: BotResponse): Promise<any> => {
-  if (content.command === commandsList.balance) {
-    const asset = (content.params[0] || DEFAULT_ASSET).toUpperCase() as TAsset | 'LIST';
+  const asset = (content.params[0] || DEFAULT_ASSET).toUpperCase() as TAsset | 'LIST';
 
-    try {
-      const dataRequest: TDataRequest = await DataForRequest.prepare();
-      const account: TResponses['account'] = await getAccount(dataRequest);
+  try {
+    const dataRequest: TDataRequest = await DataForRequest.prepare();
+    const account: TResponses['account'] = await getAccount(dataRequest);
 
-      if (asset === 'LIST') {
-        // Show list the assets with balance and >= DEFAULT_FREE
-        const balancesList: Array<TBalance> = account.balances.filter(
-          (balance: TBalance) => balance.free >= DEFAULT_FREE
-        );
+    if (asset === 'LIST') {
+      // Show list the assets with balance and >= DEFAULT_FREE
+      const balancesList: Array<TBalance> = account.balances.filter(
+        (balance: TBalance) => balance.free >= DEFAULT_FREE
+      );
 
-        let patrimony: number = 0; // Accumulator the money
+      let patrimony: number = 0; // Accumulator the money
 
-        // Creating table the balances
-        if (balancesList.length > 0) {
-          const balancesTable: TTable = balancesList.map((balance: TBalance) => {
-            patrimony += Number(balance.free) + Number(balance.locked);
+      // Creating table the balances
+      if (balancesList.length > 0) {
+        const balancesTable: TTable = balancesList.map((balance: TBalance) => {
+          patrimony += Number(balance.free) + Number(balance.locked);
 
-            const free = Utils.Float.convert(balance.free, DECIMAL_QTY_FIAT);
-            const locked = Utils.Float.convert(balance.locked, DECIMAL_QTY_FIAT);
-
-            // Adding columns in the table
-            return {
-              title: balance.asset,
-              content: `free: ${free} $ \nlocked: ${locked} $`,
-              fieldType: 'column'
-            };
-          });
-
-          // Adding a last row in the table
-          balancesTable.push({
-            title: 'Saldo de todos tus activos',
-            content: Utils.Float.convert(patrimony, DECIMAL_QTY_FIAT) + ' $',
-            fieldType: 'row'
-          });
-
-          response.embeded({
-            header: 'SALDO',
-            title: 'Lista de activos con saldo',
-            detail: balancesTable
-          });
-        } else response.general(`No hay activos con saldo > ${DEFAULT_FREE} $`);
-      } else {
-        // Show asset seleted
-        const balance = account.balances.find(
-          (balance: TBalance) => balance.asset === asset
-        );
-
-        if (balance) {
           const free = Utils.Float.convert(balance.free, DECIMAL_QTY_FIAT);
           const locked = Utils.Float.convert(balance.locked, DECIMAL_QTY_FIAT);
 
-          response.embeded({
-            header: 'SALDO',
-            title: `Saldo del activo: ${asset}`,
-            detail: `free: ${free} $ \nlocked: ${locked} $`
-          });
-        } else response.general(`No hay saldo ${asset}`);
-      }
-    } catch (error) {
-      console.error(`>> ERROR-${content.command.toUpperCase()} -> ${error}`);
+          // Adding columns in the table
+          return {
+            title: balance.asset,
+            content: `free: ${free} $ \nlocked: ${locked} $`,
+            fieldType: 'column'
+          };
+        });
+
+        // Adding a last row in the table
+        balancesTable.push({
+          title: 'Saldo de todos tus activos',
+          content: Utils.Float.convert(patrimony, DECIMAL_QTY_FIAT) + ' $',
+          fieldType: 'row'
+        });
+
+        response.embeded({
+          header: 'SALDO',
+          title: 'Lista de activos con saldo',
+          detail: balancesTable
+        });
+      } else response.general(`No hay activos con saldo > ${DEFAULT_FREE} $`);
+    } else {
+      // Show asset seleted
+      const balance = account.balances.find(
+        (balance: TBalance) => balance.asset === asset
+      );
+
+      if (balance) {
+        const free = Utils.Float.convert(balance.free, DECIMAL_QTY_FIAT);
+        const locked = Utils.Float.convert(balance.locked, DECIMAL_QTY_FIAT);
+
+        response.embeded({
+          header: 'SALDO',
+          title: `Saldo del activo: ${asset}`,
+          detail: `free: ${free} $ \nlocked: ${locked} $`
+        });
+      } else response.general(`No hay saldo ${asset}`);
     }
+  } catch (error) {
+    console.error(`>> ERROR-${content.command.toUpperCase()} -> ${error}`);
   }
 };
--- src/commands/coin.commands.ts
+++ src/commands/coin.commands.ts
@@ -1,4 +1,3 @@
-import { commandsList } from '../enumerations';
 import BotResponse from '../modules/BotResponse';
 import Utils from '../modules/Utils';
 import { getOrderBook, getPrice } from '../routes';
@@ -10,79 +9,75 @@ const DECIMAL_QTY: number = 2;
 const HIGH_QTY: number = 0.1;
 
 export const price = async (content: TContent, response: BotResponse): Promise<void> => {
-  if (content.command === commandsList.price) {
-    try {
-      const asset = (content.params[0] || 'btc').toUpperCase() as TAsset;
+  const asset = (content.params[0] || 'btc').toUpperCase() as TAsset;
 
-      const exchangePrice: number = await getPrice(asset);
+  try {
+    const exchangePrice: number = await getPrice(asset);
 
-      response.general(`Precio del ${asset} = **${exchangePrice} $**`);
-    } catch (error) {
-      console.error(`>> ERROR-${content.command.toUpperCase()} -> ${error}`);
-    }
+    response.general(`Precio del ${asset} = **${exchangePrice} $**`);
+  } catch (error) {
+    console.error(`>> ERROR-${content.command.toUpperCase()} -> ${error}`);
   }
 };
 
 export const alarm = async (content: TContent, response: BotResponse): Promise<void> => {
   // TODO: Validar multiples alarmas llevando un registro de las mismas en una DB
-  if (content.command === commandsList.alarm) {
-    const ecuation: Array<string> = content.params;
-    const sign: string = ecuation[1];
+  const ecuation: Array<string> = content.params;
+  const sign: string = ecuation[1];
 
-    if (['>', '=', '<'].includes(sign)) {
-      const asset: string = ecuation[0].toUpperCase();
-      const price: number = Number(ecuation[2]);
-      let alarmaActive: boolean = false;
+  if (['>', '=', '<'].includes(sign)) {
+    const asset: string = ecuation[0].toUpperCase();
+    const price: number = Number(ecuation[2]);
+    let alarmaActive: boolean = false;
 
-      try {
-        let exchangePrice: number = await getPrice(asset);
+    try {
+      let exchangePrice: number = await getPrice(asset);
+      if (config.modeDebug) console.log('Price = ' + exchangePrice);
+
+      // TODO: abstraer price monitor
+      const priceMonitor = setInterval(async () => {
+        exchangePrice = await getPrice(asset);
         if (config.modeDebug) console.log('Price = ' + exchangePrice);
 
-        // TODO: abstraer price monitor
-        const priceMonitor = setInterval(async () => {
-          exchangePrice = await getPrice(asset);
-          if (config.modeDebug) console.log('Price = ' + exchangePrice);
+        switch (sign) {
+          case '>':
+            if (exchangePrice >= price) alarmaActive = true;
+            break;
 
-          switch (sign) {
-            case '>':
-              if (exchangePrice >= price) alarmaActive = true;
-              break;
+          case '=':
+            if (exchangePrice == price) alarmaActive = true;
+            break;
 
-            case '=':
-              if (exchangePrice == price) alarmaActive = true;
-              break;
+          case '<':
+            if (exchangePrice <= price) alarmaActive = true;
+            break;
+        }
 
-            case '<':
-              if (exchangePrice <= price) alarmaActive = true;
-              break;
-          }
+        if (alarmaActive) {
+          response.direct(''); // TODO: Mension directa al autor del msj (provicional)
 
-          if (alarmaActive) {
-            response.direct(''); // TODO: Mension directa al autor del msj (provicional)
-
-            response.embeded({
-              header: 'ALARMA',
-              title: `Alarma ${'id'} activada`,
-              detail: `Precio del ${asset} = **${exchangePrice} $**`,
-              color: 'green'
-            });
-            clearInterval(priceMonitor);
-          }
-        }, ALARM_INTERVAL);
-      } catch (error) {
-        console.error(`>> ERROR-${content.command.toUpperCase()} -> ${error}`);
-      }
-      // TODO: Cambiar por un id corto que indentifique la alarma activada
-      // y guardarla usando redux
-      response.general(`OK yo te aviso! 👍`);
-    } else {
-      response.embeded({
-        header: 'ALARM',
-        title: 'Signo Incorrecto',
-        detail: `Solo los siguientes signos de comparación estan disponible: \`>, =, <\``,
-        color: '0xff0000'
-      });
+          response.embeded({
+            header: 'ALARMA',
+            title: `Alarma ${'id'} activada`,
+            detail: `Precio del ${asset} = **${exchangePrice} $**`,
+            color: 'green'
+          });
+          clearInterval(priceMonitor);
+        }
+      }, ALARM_INTERVAL);
+    } catch (error) {
+      console.error(`>> ERROR-${content.command.toUpperCase()} -> ${error}`);
     }
+    // TODO: Cambiar por un id corto que indentifique la alarma activada
+    // y guardarla usando redux
+    response.general(`OK yo te aviso! 👍`);
+  } else {
+    response.embeded({
+      header: 'ALARM',
+      title: 'Signo Incorrecto',
+      detail: `Solo los siguientes signos de comparación estan disponible: \`>, =, <\``,
+      color: '0xff0000'
+    });
   }
 };
 
@@ -90,55 +85,53 @@ export const orderbook = async (
   content: TContent,
   response: BotResponse
 ): Promise<void> => {
-  if (content.command === commandsList.orderbook) {
-    const asset: string = (content.params[0] || 'btc').toUpperCase();
-    const limit: number = Number(content.params[1] || DEFAULT_LIMIT);
+  const asset: string = (content.params[0] || 'btc').toUpperCase();
+  const limit: number = Number(content.params[1] || DEFAULT_LIMIT);
 
-    try {
-      const orderbook: TOrderBook = await getOrderBook(asset, limit);
+  try {
+    const orderbook: TOrderBook = await getOrderBook(asset, limit);
 
-      let bestOrderBid = '';
-      let bestOrderAsk = '';
+    let bestOrderBid = '';
+    let bestOrderAsk = '';
 
-      ['bids', 'asks'].map((_bestOrder: string) => {
-        const bestOrder = _bestOrder as 'bids' | 'asks';
-        const bestOrderList = orderbook[bestOrder];
+    ['bids', 'asks'].map((_bestOrder: string) => {
+      const bestOrder = _bestOrder as 'bids' | 'asks';
+      const bestOrderList = orderbook[bestOrder];
 
-        for (let index = 0; index < bestOrderList.length; index++) {
-          let price: string = bestOrderList[index].price;
-          price = Utils.Float.convert(price, DECIMAL_QTY);
+      for (let index = 0; index < bestOrderList.length; index++) {
+        let price: string = bestOrderList[index].price;
+        price = Utils.Float.convert(price, DECIMAL_QTY);
 
-          let qty: string = bestOrderList[index].qty;
-          qty = Number(qty) >= HIGH_QTY ? `__${qty}__` : qty;
+        let qty: string = bestOrderList[index].qty;
+        qty = Number(qty) >= HIGH_QTY ? `__${qty}__` : qty;
 
-          const formatPriceQty: string = `${price} $ - ${qty} ${asset}\n`;
+        const formatPriceQty: string = `${price} $ - ${qty} ${asset}\n`;
 
-          if (bestOrder === 'bids') bestOrderBid += formatPriceQty;
-          else bestOrderAsk += formatPriceQty;
+        if (bestOrder === 'bids') bestOrderBid += formatPriceQty;
+        else bestOrderAsk += formatPriceQty;
+      }
+    });
+
+    const fieldType: TField['fieldType'] = 'column';
+
+    response.embeded({
+      header: 'ORDERBOOK',
+      title: `Los mejores precio/cantidad en el libro de ordenes para ${asset}`,
+      detail: [
+        {
+          title: 'Bids',
+          content: bestOrderBid,
+          fieldType
+        },
+        {
+          title: 'Asks',
+          content: bestOrderAsk,
+          fieldType
         }
-      });
-
-      const fieldType: TField['fieldType'] = 'column';
-
-      response.embeded({
-        header: 'ORDERBOOK',
-        title: `Los mejores precio/cantidad en el libro de ordenes para ${asset}`,
-        detail: [
-          {
-            title: 'Bids',
-            content: bestOrderBid,
-            fieldType
-          },
-          {
-            title: 'Asks',
-            content: bestOrderAsk,
-            fieldType
-          }
-        ],
-        color: '#FF5733'
-      });
-    } catch (error) {
-      console.error(`>> ERROR-${content.command.toUpperCase()} -> ${error}`);
-    }
+      ],
+      color: '#FF5733'
+    });
+  } catch (error) {
+    console.error(`>> ERROR-${content.command.toUpperCase()} -> ${error}`);
   }
 };
